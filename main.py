c395e51b102aa9aa45cf233f7666cabdf7742b72
cqwfei <cqwfei@qq.com>
2022-10-11 13:35:11 +0800

    make_webdriver

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 
+
+import os
+
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
-
-def main():
+def make_webdriver():
     option = Options()
     # option.add_argument('--headless')
     # option.add_argument('--no-sandbox')
     # option.add_argument("--disable-dev-shm-usage")
     option.add_argument("--remote-debugging-port=9222")
-    driver = webdriver.Chrome(r"./chromedriver",chrome_options=option)
+    if os.name == 'posix':
+        web_driver_file = r"./chromedriver"
+    else:
+        web_driver_file = r"chromedriver.exe"
+
+    return webdriver.Chrome(web_driver_file,chrome_options=option)
+
+
+def main():
+
+
+    driver = make_webdriver()
     time.sleep(1)
 
     driver.get("https://www.baidu.com")
