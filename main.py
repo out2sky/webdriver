f437274b84081c520e4c57836d6f3379cc1c9c30
cqwfei <cqwfei@qq.com>
2022-10-11 13:02:24 +0800

    fix

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 
-from lib2to3.pgen2 import driver
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 
 
 def main():
-    driver = webdriver.Chrome()
+    option = Options()
+    # option.add_argument('--headless')
+    # option.add_argument('--no-sandbox')
+    # option.add_argument("--disable-dev-shm-usage")
+    option.add_argument("--remote-debugging-port=9222")
+    driver = webdriver.Chrome(r"./chromedriver",chrome_options=option)
     time.sleep(1)
 
     driver.get("https://www.baidu.com")
